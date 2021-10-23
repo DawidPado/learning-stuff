eca6604307956540c35e0f25d4befea46a1ae945
Xardas7 <taif12340@gmail.com>
2021-10-23 21:39:21 +0200

    code fix

--- ia/puzzle8/structure.py
+++ ia/puzzle8/structure.py
@@ -6,9 +6,10 @@ class Node:
     puzzle = []
     index = None
     coll = 3
-    goal=[1,2,3,4,5,6,7,8,0]
-    score=0
-    path=0
+    goal = [1, 2, 3, 4, 5, 6, 7, 8, 0]
+    score = 0
+    path = 0
+
     def __init__(self, puzzle):
         """
         :param puzzle: puzzle in the actual state
@@ -21,7 +22,7 @@ class Node:
         """
         complete = True
         if self.puzzle != self.goal:
-            complete=False
+            complete = False
         return complete
 
     def MoveToRight(self):
@@ -35,14 +36,14 @@ class Node:
             child_puzzle = Node(puzzle.copy())
             # +1 because i move (0), one pos. form initial position in the array
 
-            child_puzzle.path=self.path+1
+            child_puzzle.path = self.path + 1
             # path from the root to the new node
 
             child_puzzle.puzzle[index + 1] = 0
             child_puzzle.puzzle[index] = puzzle[index + 1]
             # switching values
             self.children.append(child_puzzle)
-            child_puzzle.parent=self
+            child_puzzle.parent = self
             # appending nodes in the tree
 
     def MoveToLeft(self):
@@ -63,14 +64,14 @@ class Node:
             child_puzzle.puzzle[index] = puzzle[index - 1]
             # switching values
             self.children.append(child_puzzle)
-            child_puzzle.parent=self
+            child_puzzle.parent = self
             # appending nodes in the tree
 
     def MoveToUp(self):
         index = self.index
         puzzle = self.puzzle
 
-        if index > self.coll-1:
+        if index > self.coll - 1:
             """
             If i am on position 0,1,2 i can move empty puzzle to the up!
             """
@@ -84,14 +85,14 @@ class Node:
             child_puzzle.puzzle[index] = puzzle[index - self.coll]
             # switching values
             self.children.append(child_puzzle)
-            child_puzzle.parent=self
+            child_puzzle.parent = self
             # appending nodes in the tree
 
     def MoveToDown(self):
         index = self.index
         puzzle = self.puzzle
 
-        if index < self.coll*(self.coll-1):
+        if index < self.coll * (self.coll - 1):
             """
             If i am on position 6,7,8 i can move empty puzzle to the down!
             """
@@ -105,7 +106,7 @@ class Node:
             child_puzzle.puzzle[index] = puzzle[index + self.coll]
             # switching values
             self.children.append(child_puzzle)
-            child_puzzle.parent=self
+            child_puzzle.parent = self
             # appending nodes in the tree
 
     def describe(self):
@@ -137,7 +138,7 @@ class Node:
     def good_position(self):
         tot = 0
         for pos in range(self.coll * self.coll):
-            if self.puzzle[pos]==self.goal[pos]:
-                tot+=1
+            if self.puzzle[pos] == self.goal[pos]:
+                tot += 1
         self.score = tot
         return tot
